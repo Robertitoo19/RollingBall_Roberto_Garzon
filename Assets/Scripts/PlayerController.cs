1734bdd5a447bad7990619f6a546c1376da25e03
Robertitoo19 <robertito191103@gmail.com>
2024-09-25 17:29:18 +0200

    IniciosJuego

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] float Velocidad;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+    }
+    void Update()
+    {
+        float movX = Input.GetAxis("Horizontal");
+        float movY = Input.GetAxis("Vertical");
+
+        Vector3 movimiento=new Vector3 (movX, -1, movY);
+
+        rb.velocity = movimiento * Velocidad;
+    }
+}
